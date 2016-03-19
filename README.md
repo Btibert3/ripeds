f420eee15798b59561a6909fcd382a2977c091ed
Btibert3 <btibert3@gmail.com>
2016-03-19 18:29:21 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# ripeds
+
+An R data-package for NCES IPEDS data. Instead of crawling the data manually or using the IPEDS package, this package simply keeps stores the data in R data frames for easy access.  All you need to do is load the survey data of choice into your environment, and you are off and running.  In addition, there will be some helper functions to calculate common KPIs, like admit rate, freshmen discount rate, etc.
+
+## Quick Start
+
+
+## Notes  
+
+I am starting with years 2002 to present.
+
+
+## Data Collected
+
+- [x] HD: Directory Info
+- [ ] 
+
+
+## To do
+
+- [ ] Add data before 2002
+
+## Last Update
+
+
+## Issues/Questions/Feature Requests
---
+++ data-raw/get-directory-info.R
@@ -0,0 +1,53 @@
+options(stringsAsFactors = F)
+
+## set some of the parameters
+YEARS = 2002:2014
+SUFFIX = "HD"
+BASE_URL = "http://nces.ed.gov/ipeds/datacenter/data/%s%s.zip"
+
+## loop and get the raw datasets
+for (YEAR in YEARS) {
+  ## build the URL
+  URL = sprintf(BASE_URL, SUFFIX, YEAR)
+  ## download the zip file
+  download.file(URL, paste0(SUFFIX, YEAR, ".zip"))
+}
+
+
+## unzip the data
+FILES = list.files(pattern = ".zip")
+for (FILE in FILES) {
+  unzip(FILE)
+}
+
+
+## loop and build the datasets
+hd = data.frame()
+FILES = list.files(pattern = ".csv")
+for (FILE in FILES) {
+  ## read in
+  tmp = readr::read_csv(FILE)
+  ## because consistency is not the NCES's concern
+  colnames(tmp) = tolower(colnames(tmp))
+  ## parse out the year
+  year = stringr::str_extract(FILE, "[0-9]{4}")
+  ## cleanup so can bind against previous years
+  tmp$survey_year = year
+  if ("gentele" %in% colnames(tmp)) tmp$gentele = as.character(tmp$gentele)
+  if ("fintele" %in% colnames(tmp)) tmp$fintele = as.character(tmp$fintele)
+  if ("admtele" %in% colnames(tmp)) tmp$admtele = as.character(tmp$admtele)
+  if ("closedat" %in% colnames(tmp)) tmp$closedat = as.character(tmp$closedat)
+  ## bind rows
+  hd = dplyr::bind_rows(hd, tmp)
+  ## cleanup
+  rm(tmp)
+}
+
+
+## cleanup the directory
+FILES_CSV = list.files(pattern = ".csv")
+FILES_ZIP = list.files(pattern = ".zip")
+file.remove(c(FILES_CSV, FILES_ZIP))
+
+## save the data
+save(hd, file="../data/hd.rda")
