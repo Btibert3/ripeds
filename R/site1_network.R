9cfa3ec55b9339e4821ce7c67956c71e0cf01056
Btibert3 <btibert3@gmail.com>
2016-04-05 21:35:48 -0400

    added network data

---
+++ R/site1_network.R
@@ -0,0 +1,11 @@
+#' A crawl of the network of data from a College Search site.
+#'
+#' The crawl occurred on April 3rd, 2016.
+#' 
+#' The data connects schools (Other schools to Consider) and are based on UNITID,  Majors are represented by CIP Codes.
+#'
+#' @source myplan.com
+#' @format 2 data frames.  Links (edgelist, with rank order shown), and majors (schools and majors offered)
+#' @name site1_network
+#' @usage site1_network
+NULL
---
+++ R/site2_network.R
@@ -0,0 +1,11 @@
+#' A crawl of the network of data from a College Search site.
+#'
+#' The crawl occurred on April 3rd, 2016.
+#' 
+#' The data connects schools (Similar) and are based on UNITID,  The school's mission statement, when available (via the site) are also included
+#' 
+#' @source cappex.com
+#' @format 2 data frames.  edges (edgelist, with rank order shown), and mission statements (schools and mission statement text)
+#' @name site2_network
+#' @usage site2_network
+NULL
