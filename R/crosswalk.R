be3cd1f57bbc5139a3c95ee45c6c664c558d40c1
Btibert3 <btibert3@gmail.com>
2016-04-10 11:57:00 -0400

    added crosswalk
    dataset to help link various codes across all sources

---
+++ R/crosswalk.R
@@ -0,0 +1,11 @@
+#' Crosswalk of various codes a US-based Higher Education institution may have
+#'
+#' These codes allow analysts to join datasets from the College Board, ACT, and various Department of Ed codes
+#' 
+#' 
+#'
+#' @source Original data from: http://www.pesc.org/interior.php?page_id=145
+#' @format Data frame with columns
+#' @name crosswalk
+#' @usage crosswalk
+NULL
